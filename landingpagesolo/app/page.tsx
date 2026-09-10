1b692464efcf36dcd4d45a412e8ffb45c0ae0385
AnushkaAbhilash <anushkaabhilash2@gmail.com>
2026-09-10 15:07:52 +0530

    Animation

--- landingpagesolo/app/page.tsx
+++ landingpagesolo/app/page.tsx
@@ -188,7 +188,10 @@ export default function Home() {
           <div className="journey-decoration journey-decoration-one"></div>
           <div className="journey-decoration journey-decoration-two"></div>
           <div className="journey-decoration journey-decoration-three"></div>
-          <div className="journey-decoration journey-decoration-four"></div>
+          <div className="journey-decoration journey-decoration-four"></div><br />
+          <div className="journey-button">  <Link href="/signup" className="hero-primary">
+                Begin Your Journey
+                </Link></div>
         </section>
       </FadeInSection>
 
